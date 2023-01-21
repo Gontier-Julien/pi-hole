fbca3691b0fe2ee355f7d5cbf2c49a500d5f99d6
Gontier Julien <gontierjulien68@gmail.com>
2023-01-21 19:02:59 +0100

    Please work?
    Signed-off-by: Gontier Julien <gontierjulien68@gmail.com>

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -2256,10 +2256,9 @@ FTLinstall() {
 
         # If we downloaded binary file (as opposed to text),
         if is_command apk ; then
-            sha1sum -s -c "${binary}".sha1; then
-        else
-            sha1sum --status --quiet -c "${binary}".sha1; then
-        fi
+                sha1sum -s -c "${binary}".sha1; then
+            else
+                sha1sum --status --quiet -c "${binary}".sha1; t
             printf "transferred... "
 
             # Before stopping FTL, we download the macvendor database
